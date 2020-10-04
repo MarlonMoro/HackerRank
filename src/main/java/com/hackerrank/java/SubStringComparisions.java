031b538f4c8483d134c82fed35b27d72b0bd6017
Marlon Moro <marlon@ciclic.com.br>
2020-10-04 18:46:07 -0300

    Uma solução com 1 unico laco

--- src/main/java/com/hackerrank/java/SubStringComparisions.java
+++ src/main/java/com/hackerrank/java/SubStringComparisions.java
@@ -1,31 +1,22 @@
 package com.hackerrank.java;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class SubStringComparisions {
 
 
   public static String getSmallestAndLargest(String s, int k) {
+    String smallest = s.substring(0, k);
+    String largest = s.substring(0, k);
 
-    int limite = s.length()-k;
-
-    String[] stringVector = new String[limite+1];
-
-    for(int i = 0; i <= limite; i++){
-      stringVector[i] = s.substring(i, k+i);
-    }
-
-    String smallest = stringVector[0];
-    String largest = stringVector[0];
-    for(String str : stringVector){
-      if(smallest.compareTo(str) > 0){
-        smallest = str;
+    for(int i = 0; i +k <= s.length(); i++){
+      if(smallest.compareTo(s.substring(i, k+i)) > 0){
+        smallest = s.substring(i, k+i);
       }
-      if(largest.compareTo(str) < 0){
-        largest = str;
+      if(largest.compareTo(s.substring(i, k+i)) < 0){
+        largest = s.substring(i, k+i);
       }
+
     }
 
 
@@ -36,18 +27,6 @@ public class SubStringComparisions {
     return smallest + "\n" + largest;
   }
 
-  public static String streamSolution(String s, int k) {
-    // This challenge do not accept Stream
-    int limite = s.length()-k;
-    List<String> subsString = new ArrayList<>();
-    for(int i = 0; i <= limite; i++){
-      subsString.add(s.substring(i, k+i));
-    }
-    String largest = subsString.stream().max(String::compareTo).orElse("max");
-    String smallest = subsString.stream().min(String::compareTo).orElse("min");
-
-    return smallest + "\n" + largest;
-  }
 
 
   public static void main(String[] args) {
